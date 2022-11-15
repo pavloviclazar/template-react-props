443f587a16d6379d13420bde9f5365ad786b8453
Lazar Pavlovic <pavloviclazar2003@gmail.com>
2022-11-15 22:43:43 +0100

    poslenja verzija

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useState, useCallback } from "react";
+
+import Button from "./components/Button/Button";
+import "./App.css";
+import DemoOutput from "./components/Demo/DemoOutput";
+
+function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
+
+  const toggleParagraphHandler = useCallback(() => {
+    if (allowToggle) {
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHenlder = () => {
+    setAllowToggle(true);
+  };
+
+  return (
+    <div className="app">
+      <h1>Hi there!</h1>
+      <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHenlder}>Allow Toggling</Button>
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
+    </div>
+  );
+}
+
+export default App;
